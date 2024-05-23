ae85ca9c7eb8d6c37afe40eed9f641bb1e8a1bff
Adria Blancafort <adriablancafort@gmail.com>
2024-05-23 11:07:37 +0200

    added optional type and error for file load

--- rutes-i-monuments/monuments.py
+++ rutes-i-monuments/monuments.py
@@ -1,4 +1,4 @@
-from typing import TypeAlias
+from typing import TypeAlias, Optional
 from dataclasses import dataclass
 from bs4 import BeautifulSoup
 from requests import get
@@ -35,7 +35,7 @@ def dms_to_decimal(degrees: float, minutes: float, seconds: float) -> float:
     return round(decimal, 7)
 
 
-def calculate_coordinates(text: str) -> Point | None:
+def calculate_coordinates(text: str) -> Optional[Point]:
     """Given the location text, returns a Point with the coordinates of the location."""
 
     numbers = extract_numbers(text)
@@ -43,21 +43,22 @@ def calculate_coordinates(text: str) -> Point | None:
         # No s'han trobat coordenades
         return None
     elif len(numbers) < 6:
-        # Test amb coordenades en format decimal
+        # Coordenades en format decimal
         lat, lon = numbers[:2]
     else:
-        # Text amb coordenades en format DMS
+        # Coordenades en format DMS
         lat_deg, lat_min, lat_sec, lon_deg, lon_min, lon_sec = numbers[:6]
         lat = dms_to_decimal(lat_deg, lat_min, lat_sec)
         lon = dms_to_decimal(lon_deg, lon_min, lon_sec)
     return Point(lat, lon)
 
 
-def scrape_monument(url: str) -> Monument | None:
+def scrape_monument(url: str) -> Optional[Monument]:
     """Given the url of a monument, returns its title and coordinates."""
 
     try:
         response = get(url, timeout=8)
+    # Retry connection to improve reliability
     except (ConnectionError, Timeout, ReadTimeout):
         print(f"Request to {url} timed out. Trying again.")
         try:
@@ -80,10 +81,10 @@ def scrape_monument(url: str) -> Monument | None:
     for element in location_elements:
         if element.find("strong", string="Localització"):
             location_link = element.find("a")
-            # The coordinates are inside a link
+            # The coordinates are inside a link (<a></a>)
             if location_link:
                 location = location_link.text
-            # The coordinates are inside the paragraph
+            # The coordinates are inside the paragraph (<p></p>)
             else:
                 location = element.text
     if location:
@@ -134,18 +135,19 @@ def write_monuments_to_file(monuments: Monuments, filename: str) -> None:
         dump(monuments, f)
 
 
-def read_monuments_from_file(filename: str) -> Monuments | None:
+def read_monuments_from_file(filename: str) -> Optional[Monuments]:
     """Read the list of monuments from a file."""
 
     try:
         with open(filename, "rb") as f:
             monuments = load(f)
         return monuments
-    except:
+    except FileNotFoundError:
+        print(f"File {filename} not found.")
         return None
 
 
-def get_monuments(filename: str) -> Monuments | None:
+def get_monuments(filename: str) -> Optional[Monuments]:
     """
     Get all monuments in the box.
     If filename exists, load monuments from the file.
@@ -157,6 +159,3 @@ def get_monuments(filename: str) -> Monuments | None:
         monuments = download_monuments()
         write_monuments_to_file(monuments, filename)
     return monuments
-
-
-# get_monuments("monuments.dat")
